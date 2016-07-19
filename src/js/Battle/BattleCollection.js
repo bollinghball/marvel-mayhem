e53bd13f020ad2ec19d6a623df1fa064fd387306
Bolling Ball <bollinghball@gmail.com>
2016-07-19 10:54:51 -0400

    fixed backbone error

--- src/js/Battle/BattleCollection.js
+++ src/js/Battle/BattleCollection.js
@@ -2,7 +2,7 @@ var Backbone = require('backbone');
 
 var BattleModel = require('./BattleModel');
 
-var BattleCollection = new Backbone.Collection.extend({
+var BattleCollection = Backbone.Collection.extend({
 	model: BattleModel,
 	url: 'http://localhost:8000/battles'
 });
